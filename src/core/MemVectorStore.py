bc3d9b8838e022c0d0149ccba3b4ef10917adcfb
Anand Awasthi <anandawasthi89@gmail.com>
2026-04-25 13:29:33 +0530

    clearing mem_vector_db

--- src/core/MemVectorStore.py
+++ src/core/MemVectorStore.py
@@ -1,4 +1,5 @@
 from langchain_community.vectorstores import Chroma
+from langchain_core.documents import Document
 from src.core.Sanitisers import normalize_pdf_name
 import chromadb
 
@@ -7,8 +8,12 @@ class MemVectorStore:
     def __init__(self, embedding_service):
         self.embedding_function = embedding_service.embeddings_model
         self.client = chromadb.Client()
-        self.vectordb = Chroma(
+
+    def add_documents(self, chunks: list[Document]):
+        vector_db = Chroma(
             client=self.client,
             collection_name="mem_collection",
             embedding_function=self.embedding_function,
         )
+        return vector_db
+
